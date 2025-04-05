dff5f5b2928e6e1f90616b993acd7d8ed9c1b84e
Rachel-peng <2364352685@qq.com>
2025-04-05 17:26:56 +0800

    useful charts.

--- src/Components/DemoPie/index.tsx
+++ src/Components/DemoPie/index.tsx
@@ -4,12 +4,10 @@ import { Pie } from '@ant-design/plots';
 const DemoPie: React.FC = () => {
     const config = {
         data: [
-            { type: '分类一', value: 27 },
-            { type: '分类二', value: 25 },
-            { type: '分类三', value: 18 },
-            { type: '分类四', value: 15 },
-            { type: '分类五', value: 10 },
-            { type: '其他', value: 5 },
+            { type: '是何问题', value: 27 },
+            { type: '为何问题', value: 25 },
+            { type: '如何问题', value: 18 },
+            { type: '若何问题', value: 18 },
         ],
         angleField: 'value',
         colorField: 'type',
@@ -31,7 +29,7 @@ const DemoPie: React.FC = () => {
             {
                 type: 'text',
                 style: {
-                    text: 'AntV\nCharts',
+                    text: 'questioning\ntype',
                     x: '50%',
                     y: '50%',
                     textAlign: 'center',
